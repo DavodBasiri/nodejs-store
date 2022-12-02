c7d8f6206c14c7a619933e8554313fb38a00481c
github_username <email_address>
2022-12-02 12:34:49 +0330

    new

--- app/models/blogs.js
+++ app/models/blogs.js
@@ -1,17 +1,25 @@
 const mongoose = require("mongoose");
-
-const Schema = new mongoose.Schema({
-    author : {type : mongoose.Types.ObjectId,required :true},
-    title : {type :String,required :true},
-    text : {type : String,required :true},
-    image : {type : String,required :true},
-    tags : {type : [String],default :[]},
-    category : {type : mongoose.Types.ObjectId, required :true},
-    comments : {type : [], default :[]},
-    like : {type : [mongoose.Types.ObjectId], default :[] },
-    deslike : {type : [mongoose.Types.ObjectId], default :[]},
-    bookmark : {type : [mongoose.Types.ObjectId], default :[]}
+const CommentsSchema = new mongoose.Schema({
+  user: { type: mongoose.Types.ObjectId, required: true, ref: "users" },
+  comment: { type: String, required: true },
+  timestamp: { type: Date, default: new Date.now() },
+  parent: { type: mongoose.Types.ObjectId, default: undefined }
 });
-module.exports ={
-    BlogModel : mongoose.model("blog",Schema)
-}
+const Schema = new mongoose.Schema({
+  author: { type: mongoose.Types.ObjectId, required: true },
+  title: { type: String, required: true },
+  text: { type: String, required: true },
+  short_text: { type: String, required: true },
+  image: { type: String, required: true },
+  tags: { type: [String], default: [] },
+  category: { type: mongoose.Types.ObjectId, required: true },
+  comments: { type: [CommentsSchema], default: [] },
+  like: { type: [mongoose.Types.ObjectId], ref: "users", default: [] },
+  deslike: { type: [mongoose.Types.ObjectId], ref: "users", default: [] },
+  bookmark: { type: [mongoose.Types.ObjectId], ref: "users", default: [] },
+},{
+    timestamps:ture,versionKey:false
+});
+module.exports = {
+  BlogModel: mongoose.model("blog", Schema),
+};
